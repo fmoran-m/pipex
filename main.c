d7db7f212da5a8de6547cc36cb785aec1e122fc1
Francisco Jose Moran Mato <fmoran-m@c1r5s4.42madrid.com>
2024-02-19 20:48:24 +0100

    It works without files as arguments

--- main.c
+++ main.c
@@ -4,14 +4,18 @@ char *get_path(char *argv, char **env)
 {
 	int		i;
 	char	**path;
+	char	*temp;
 	char	*search;
 	char	*command;
 
+	i = 0;
 	command = ft_strjoin("/", argv);
 	while (env[i] && (ft_strncmp(env[i], "PATH=", 5)))
 		i++;
-	env[i] = env[i] + 5;
-	path = ft_split(env[i], ':'); //Free
+	if (!env[i])
+		exit(-1);
+	temp = env[i] + 5;
+	path = ft_split(temp, ':'); //Free
 	i = 0;
 	search = ft_strjoin(path[i], command);
 	while (path[i] && access(search, F_OK) != 0)
@@ -25,21 +29,50 @@ char *get_path(char *argv, char **env)
 
 int main(int argc, char **argv, char **env)
 {
-	char	*cmd1;
-	char	*cmd2;
+	char	**cmd1;
+	char	**cmd2;
+	char	*path1;
+	char	*path2;
 	pid_t	pid;
+	int		infile;
+	int		outfile;
 	int		fd[2];
 
-	cmd1 = get_path(argv[2], env);
-	cmd2 = get_path(argv[3], env);
+	if (argc != 3)
+	{
+		ft_putendl_fd("Incorrect number of arguments", 2);
+		return (-1);
+	}
+	cmd1 = ft_split(argv[1], ' ');
+	cmd2 = ft_split(argv[2], ' ');
+	path1 = get_path(cmd1[0], env);
+	path2 = get_path(cmd2[0], env);
+	infile = open("infile.txt", O_RDWR)
+	outfile = open("outfile.txt", O_RDWR);
+	dup2(infile , 0); //Cogemos la info del infile
 	pipe(fd);
 	pid = fork();
 	if (pid == 0)
 	{
 		close(fd[0]);
-		dup2(fd[1], 1);
+		dup2(fd[1], 1); //Vamos a ejecutar el comando. Printeamos en el fd[1] en lugar de en la salida estándar
 		close(fd[1]);
-		execve(cmd1, NULL, env);
+		execve(path1, cmd1, env);
+	}
+	else
+	{
+		close(fd[1]);
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(fd[0], 0); //Recogemos de la pipe
+			close(fd[0]);
+			dup2(outfile, 1); //Lo llevamos al archivo final
+			execve(path2, cmd2, env);
+		}
+		else
+			close(fd[0]);
+
 	}
 	return 0;
 }
